1af335807a2e903f839acf315f53e6a9d4f0fc35
Hugo César Leal <hugocesarleal@gmail.com>
2025-01-27 10:30:12 -0300

    Delete trabalho_registro_projetos directory

--- trabalho_registro_projetos/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function formatarMoeda(event) {
-    var valor = event.target.value;
-    valor = valor.replace(/\D/g, '');
-    valor = valor.replace(/(\d)(\d{2})$/, '$1,$2');
-    if (valor.length > 3) {
-        valor = valor.replace(/(\d)(\d{3})(\d{1,2}$)/, '$1.$2,$3');
-    }
-    event.target.value = 'R$ ' + valor;
-}
